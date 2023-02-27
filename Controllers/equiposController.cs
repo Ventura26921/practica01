a4f9e9823484a81a351f4f5c5c8e0010a888319e
alexa <alexa@ALEX>
2023-02-26 19:25:19 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/equiposController.cs
@@ -0,0 +1,126 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using practica01.Models;
+
+
+namespace practica01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+        }
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos
+                                            select e).ToList();
+            if (listadoEquipo.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoEquipo);
+        }
+        [HttpGet]
+        [Route("GetById/{id}")]
+
+        public IActionResult Get(int id)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipo);
+        }
+        [HttpGet]
+        [Route("find/{filtro}")]
+
+        public IActionResult FinfByDescription(string filtro)
+        {
+            List<equipos> equipos = (from e in _equiposContexto.equipos
+                                     where e.descripcion.Contains(filtro)
+                                     select e).ToList();
+            if (equipos == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipos);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarEquipo([FromBody] equipos equipos)
+        {
+            try
+            {
+                _equiposContexto.equipos.Add(equipos);
+                _equiposContexto.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar)
+        {
+            equipos? equipoActual = (from e in _equiposContexto.equipos
+                                     where e.id_equipos == id
+                                     select e).FirstOrDefault();
+            if (equipoActual == null)
+            {
+                return NotFound();
+            }
+            equipoActual.nombre = equipoModificar.nombre;
+            equipoActual.descripcion = equipoModificar.descripcion;
+            equipoActual.marca_id = equipoModificar.marca_id;
+            equipoActual.tipo_equipos_id = equipoModificar.tipo_equipos_id;
+            equipoActual.anio_compra = equipoModificar.anio_compra;
+            equipoActual.costo = equipoModificar.costo;
+
+            _equiposContexto.Entry(equipoActual).State = EntityState.Modified;
+            _equiposContexto.SaveChanges();
+
+            return Ok(equipoModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+
+        public IActionResult EliminarEquipo(int id)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            _equiposContexto.Attach(equipo);
+            _equiposContexto.Remove(equipo);
+            _equiposContexto.SaveChanges();
+            return Ok(equipo);
+        }
+    }
+}
